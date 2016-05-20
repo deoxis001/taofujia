1ad517599a63b3233a709c0ac29d800b18eda827
bdsiq <1042921964@qq.com>
2016-05-20 13:18:46 +0800

    231

---
+++ delivery/control/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 物流自提服务站首页
+ *
+
+ */
+
+
+
+defined('ByShopWWI') or exit('Access Invalid!');
+
+class indexControl extends BaseDeliveryControl{
+    public function __construct(){
+        parent::__construct();
+        @header('location: index.php?act=login');die;
+    }
+}
