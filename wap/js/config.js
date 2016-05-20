7fbc06cc030d197428c4723cb643f52393a4dedf
bdsiq <1042921964@qq.com>
2016-05-20 13:56:44 +0800

    231

---
+++ wap/js/config.js
@@ -0,0 +1,7 @@
+var SiteUrl = "/shop";
+var ApiUrl = "/mobile";
+var pagesize = 10;
+var WapSiteUrl = "/wap";
+var IOSSiteUrl = "https://itunes.apple.com/us/app/shopnc-b2b2c/id879996267?l=zh&ls=1&mt=8";
+var AndroidSiteUrl = "/download/app/AndroidShopNCMoblie.apk";
+var WeiXinOauth = false;
