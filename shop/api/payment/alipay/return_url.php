d24ea5a796a484e857db3ef453c58103a707d3e1
bdsiq <1042921964@qq.com>
2016-05-20 13:46:29 +0800

    231

---
+++ shop/api/payment/alipay/return_url.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * 支付宝返回地址
+ */
+error_reporting(7);
+$_GET['act']	= 'payment';
+$_GET['op']		= 'return';
+$_GET['payment_code'] = 'alipay';
+require_once(dirname(__FILE__).'/../../../index.php');
+?>
